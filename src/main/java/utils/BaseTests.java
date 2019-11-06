7adfb1b3bcec86750f44a7d8ce1d72b8ac08c02a
Giuseppe Torchia <giuseppe.torchia1@nhs.net>
2019-11-06 10:50:01 +0000

    modified BaseTest and pom file

--- src/main/java/utils/BaseTests.java
+++ src/main/java/utils/BaseTests.java
@@ -6,11 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageobject.HomePage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+
 
 public class BaseTests {
 
