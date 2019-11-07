314e6ea50ed1c75d61aedaab74d7b90cf87dce9b
Giuseppe Torchia <giuseppe.torchia1@nhs.net>
2019-11-07 13:16:31 +0000

    unit test for propertyloader

---
+++ src/main/java/test/PropertyLoaderTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertyLoaderTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void loadProperty() {
+
+    }
+
+    @Test
+    public void testLoadProperty() {
+    }
+}
